0b4b11e830e92e7140bf982979978b25b6f79707
Isabelle K <ikrochma@ucsd.edu>
2022-10-29 15:32:22 -0700

    Party Horn first attempt

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,62 @@
-// expose.js
+//expose.js
 
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const dropdown = document.querySelector('select');
+  //console.log(dropdown);
+  const hornImage = document.querySelector('alt="No image selected"');
+  //console.log(hornImage);
+  const hornAudio = document.querySelector('audio');
+  //console.log(hornAudio);
+  const volumeImage = document.querySelector('alt="Volume level 2"');
+  //console.log(volumeImage);
+  const slider = document.querySelector('type = "range"');
+  //console.log(slider);
+  const playButton = document.quertSelector('button');
+  //console.log(playButton);
+  const confetti = new JSConfetti();
+
+  dropdown.addEventListener('change', setHorn);
+  slider.addEventListener('change', setVolume);
+  playButton.addEventListener('click', playHorn);
+
+  function setHorn() {
+    if (dropdown.value == "air-horn") {
+      hornAudio.src = "assets/audio/air-horn.mp3";
+      hornImage.src = "assets/images/air-horn.svg";
+    }
+    if (dropdown.value == "car-horn") {
+      hornAudio.src = "assets/audio/car-horn.mp3";
+      hornImage.src = "assets/images/car-horn.svg";
+    }
+    if (dropdown.value == "party-horn") {
+      hornAudio.src = "assets/audio/party-horn.mp3";
+      hornImage.src = "assets/images/party-horn.svg";
+    }
+  }
+
+  function setVolume() {
+    let volume = slider.value;
+    if (volume == 0){
+      volumeImage.src = "assets/audio/volume-level-0.svg";
+    }
+    else if (volume < 33) {
+      volumeImage.src = "assets/audio/volume-level-1.svg";
+    }
+    else if (volume < 67) {
+      volumeImage.src = "assets/audio/volume-level-2.svg";
+    }
+    else {
+      volumeImage.src = "assets/audio/volume-level-3.svg";
+    }
+    hornAudio.volume = volume / 100;
+  }
+
+  function playHorn() {
+    hornAudio.play();
+    if (hornAudio.src == "assets/audio/party-horn.mp3") {
+      confetti.addConfetti();
+    }
+  }
 }
